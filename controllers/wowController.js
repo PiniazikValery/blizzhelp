9fefd98d436a14830fed6e3c392a186e3a2aadc1
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2018-07-27 17:11:36 +0300

    AH[7] - added tanking guide

--- controllers/wowController.js
+++ controllers/wowController.js
@@ -1,3 +1,7 @@
-exports.get_wow = function(req, res) {
-    res.render('wow/index',{title:'Blizzhelp',dropdownFor:'wow'});
+exports.get_wow = function (req, res) {
+    res.render('wow/index', { title: 'Blizzhelp', dropdownFor: 'wow' });
+};
+
+exports.get_tanking_guide = function (req, res) {
+    res.render('wow/general_guides/tanking_guide', { dropdownFor: 'wow' });
 };
--- routes/wow.js
+++ routes/wow.js
@@ -5,4 +5,6 @@ var wowController = require('../controllers/wowController');
 
 router.get('/',wowController.get_wow);
 
+router.get('/tanking_guide',wowController.get_tanking_guide);
+
 module.exports = router;
