e966bac63efd6655b33f0c064e769a8dfc597e2f
Valery Piniazik <valery.piniazik@itechart-group.com>
2018-10-18 18:22:03 +0300

    RP[1] - added redux-persist

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,21 @@
 import thunkMiddleware from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(thunkMiddleware));
+
+const persistor = persistStore(store);
+
+export {
+  store,
+  persistor,
+};
