7317a4d02efe53f0b7ace9695eefa6fc5b765c4d
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2018-10-31 17:44:42 +0300

    SUAC[1] - started seting up ui auth component

--- client/src/react-components/layout-components/account_navbar_component.js
+++ client/src/react-components/layout-components/account_navbar_component.js
@@ -8,6 +8,40 @@ const mapStateToProps = state => ({
 });
 
 class AccountNavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loginDropDownOpened: false };
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.handleLogInClick = this.handleLogInClick.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+
+  handleLogInClick() {
+    this.setState(state => ({
+      loginDropDownOpened: !state.loginDropDownOpened,
+    }));
+  }
+
+  handleClickOutside(event) {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      this.setState({
+        loginDropDownOpened: false,
+      });
+    }
+  }
+
   render() {
     switch (this.props.isUserAuthenticated) {
       case true:
@@ -16,11 +50,14 @@ class AccountNavBar extends Component {
         );
       default:
         return (
-          <div id="log_in">
-            <a href="/">
+          <div ref={this.setWrapperRef}>
+            <div role="button" tabIndex="0" id="log_in" onClick={this.handleLogInClick} onKeyDown={this.handleLogInClick}>
               <img id="sprite_log_in" className="sprite_auth" alt="" />
               <span>Войти</span>
-            </a>
+            </div>
+            <div>
+              {this.state.loginDropDownOpened ? 'opened' : 'closed'}
+            </div>
           </div>
         );
     }
