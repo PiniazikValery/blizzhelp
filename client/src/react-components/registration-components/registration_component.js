c1417585f8c95aa30299f7c9e5427bd722251773
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-02-19 20:02:42 +0300

    SUAC - Added highlights of fields with misstakes

--- client/src/react-components/registration-components/registration_component.js
+++ client/src/react-components/registration-components/registration_component.js
@@ -13,6 +13,11 @@ class Registration extends Component {
       rewrited_password: '',
       password: '',
       auth_code: '',
+      wrong_email: false,
+      wrong_username: false,
+      wrong_rewrited_password: false,
+      wrong_password: false,
+      wrong_auth_code: false,
     };
     this._reCaptchaRef = React.createRef();
     this.handleEmailTyping = this.handleEmailTyping.bind(this);
@@ -22,6 +27,7 @@ class Registration extends Component {
     this.handleAuthCodeTyping = this.handleAuthCodeTyping.bind(this);
     this.onClickGenerateCode = this.onClickGenerateCode.bind(this);
     this.validateFields = this.validateFields.bind(this);
+    this.removeAllWarnings = this.removeAllWarnings.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
   }
@@ -57,6 +63,7 @@ class Registration extends Component {
   }
 
   onSubmit = () => {
+    this.removeAllWarnings();
     this.validateFields((validationError) => {
       if (validationError) {
         notificators.mainNotificator.showWarning(validationError.message);
@@ -96,21 +103,40 @@ class Registration extends Component {
 
   validateFields(callback) {
     if (!this.state.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+      this.setState({
+        wrong_email: true,
+      });
       return callback(new Error('Invalid email'));
     }
     if (this.state.username === '') {
+      this.setState({
+        wrong_username: true,
+      });
       return callback(new Error('Empty username field'));
     }
     if (this.state.password !== this.state.rewrited_password) {
+      this.setState({
+        wrong_password: true,
+        wrong_rewrited_password: true,
+      });
       return callback(new Error('Passwords dosen`t matchs'));
     }
     if (this.state.password === '') {
+      this.setState({
+        wrong_password: true,
+      });
       return callback(new Error('Empty password field'));
     }
     if (this.state.password.length <= 8) {
+      this.setState({
+        wrong_password: true,
+      });
       return callback(new Error('Password lenght lower then 8 sumbols'));
     }
     if (this.state.auth_code === '') {
+      this.setState({
+        wrong_auth_code: true,
+      });
       return callback(new Error('Empty code field'));
     }
     return callback();
@@ -146,6 +172,16 @@ class Registration extends Component {
     });
   }
 
+  removeAllWarnings() {
+    this.setState({
+      wrong_email: false,
+      wrong_username: false,
+      wrong_password: false,
+      wrong_rewrited_password: false,
+      wrong_auth_code: false,
+    });
+  }
+
   render() {
     return (
       <div id="registrationMenu">
@@ -159,20 +195,20 @@ class Registration extends Component {
           <h4>Registration from</h4>
           <ul id="menuFields">
             <li>
-              <input className="longField" placeholder="Email" id="registrationEmail" value={this.state.email} onChange={this.handleEmailTyping} />
+              <input className={`longField ${this.state.wrong_email ? 'wrongEntered' : ''}`} placeholder="Email" id="registrationEmail" value={this.state.email} onChange={this.handleEmailTyping} />
             </li>
             <li>
-              <input className="longField" placeholder="Username" id="registrationUsername" value={this.state.username} onChange={this.handleUsernameTyping} />
+              <input className={`longField ${this.state.wrong_username ? 'wrongEntered' : ''}`} placeholder="Username" id="registrationUsername" value={this.state.username} onChange={this.handleUsernameTyping} />
             </li>
             <li>
-              <input className="longField" type="password" placeholder="Password" id="registrationPassword" value={this.state.password} onChange={this.handlePasswordTyping} />
+              <input className={`longField ${this.state.wrong_password ? 'wrongEntered' : ''}`} type="password" placeholder="Password" id="registrationPassword" value={this.state.password} onChange={this.handlePasswordTyping} />
             </li>
             <li>
-              <input className="longField" type="password" placeholder="Re-enter password" id="registrationRewritedPassword" value={this.state.rewrited_password} onChange={this.handlePasswordRewriting} />
+              <input className={`longField ${this.state.wrong_rewrited_password ? 'wrongEntered' : ''}`} type="password" placeholder="Re-enter password" id="registrationRewritedPassword" value={this.state.rewrited_password} onChange={this.handlePasswordRewriting} />
             </li>
             <li>
               <button id="GenerateCodeButton" type="button" onClick={this.onClickGenerateCode}>Send code</button>
-              <input className="shortField" placeholder="Code" id="registrationAuthCode" value={this.state.auth_code} onChange={this.handleAuthCodeTyping} />
+              <input className={`shortField ${this.state.wrong_auth_code ? 'wrongEntered' : ''}`} placeholder="Code" id="registrationAuthCode" value={this.state.auth_code} onChange={this.handleAuthCodeTyping} />
             </li>
           </ul>
           <button id="SubmitButton" type="button" onClick={this.onSubmit}>Create user</button>
