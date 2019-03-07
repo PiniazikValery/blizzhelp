09b374cab03829fcdd605332e53adb236697d837
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-03-07 12:20:46 +0300

    GALU - implemented article list ui

--- client/src/react-components/blog-components/article_element_component.js
+++ client/src/react-components/blog-components/article_element_component.js
@@ -12,7 +12,7 @@ class ArticleElement extends Component {
   render() {
     return (
       <div className="articleElement">
-        {this.state.hasImage ? <img alt="HTML5 Icon" src={`/blog_api/article_image/${this.props.articleId}`} className="article_image" /> : undefined}
+        {this.state.hasImage ? <img alt="HTML5 Icon" src={`/blog_api/article_image/${this.props.articleId}`} className="article_image" /> : <img alt="HTML5 Icon" src="/images/icons/blog/image_not_found.png" className="article_image" />}
         <div className="articleInfo">
           <span className="title">{this.props.title}</span>
           <span className="preViewContent">{this.props.preViewContent}</span>
